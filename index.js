e471783656146cd4dabbe43aa8a4775e6db80e9c
VivekKnit705 <vivek.knit69@gmail.com>
2022-03-25 11:08:06 +0530

    Calculator Created with JavaScript Completed

---
+++ index.js
@@ -0,0 +1,89 @@
+let result = document.getElementById("screen");
+button = document.querySelectorAll("button");
+let updateTextBox = '';
+let pre = 0;
+let srt = 0;
+for (item of button) {
+    item.addEventListener('click', (e) => {
+        inputText = e.target.innerText;
+
+
+        // Here we are checking inputText=='pyt'
+        if (inputText == 'pyt') {
+            result.value = updateTextBox + inputText;
+            srt = eval(updateTextBox) * eval(updateTextBox);
+            updateTextBox = "";
+            result.value = updateTextBox;
+            pre = 1;
+        }
+        // Here we are check pre==1 or inputText=='=' then we calculate the value ans make the pre==0;
+
+        else if (inputText == '=' && pre == 1) {
+            srt = Math.sqrt(srt + eval(updateTextBox) * eval(updateTextBox));
+            updateTextBox = "";
+            updateTextBox += srt;
+            result.value = updateTextBox;
+            pre = 0;
+        }
+        else if (inputText === 'X') {
+            inputText = '*';
+            updateTextBox += inputText;
+            result.value = updateTextBox;
+        }
+        else if (inputText == '=') {
+            updateTextBox = eval(updateTextBox).toString()
+            result.value = updateTextBox;
+        }
+        else if (inputText == 'C') {
+            updateTextBox = "";
+            result.value = updateTextBox;
+        }
+        else if (inputText == '←') {
+            updateTextBox = updateTextBox.slice(0, -1);
+            result.value = updateTextBox;
+        }
+        else if (inputText == "sin") {
+            var x = eval(result.value);
+            updateTextBox = (x * (Math.PI / 180)).toString();
+            result.value = Math.sin(updateTextBox);
+
+        }
+        else if (inputText == "cos") {
+            var x = eval(result.value);
+            updateTextBox = x * (Math.PI / 180).toString();
+            result.value = Math.cos(updateTextBox);
+
+        }
+        else if (inputText == "tan") {
+            var x = eval(result.value);
+            updateTextBox = x * (Math.PI / 180).toString();
+            result.value = Math.tan(updateTextBox);
+            // screenValue = screen.value
+        }
+        else if (inputText == 'sqrt') {
+            var x = eval(result.value);
+            updateTextBox = Math.sqrt(x).toString();
+            result.value = updateTextBox;
+
+        }
+        else if (inputText == 'log') {
+            var x = eval(result.value);
+            updateTextBox = Math.log(x).toString();
+            result.value = updateTextBox;
+        }
+        else if (inputText == 'exp') {
+            var x = eval(result.value);
+            updateTextBox = Math.exp(x).toString();
+            result.value = updateTextBox;
+        }
+        else if (inputText == '^') {
+            var x = eval(result.value);
+            updateTextBox = Math.pow(x).toString();
+            result.value = updateTextBox;
+        }
+        else {
+            updateTextBox += inputText;
+            result.value = updateTextBox;
+        }
+    })
+}
